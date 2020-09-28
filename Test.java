db96fa7feb8bdbce097e9c217913eab72ba99959
MeroMerro <72040425+MeroMerro@users.noreply.github.com>
2020-09-28 21:17:00 +0300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+    public static void main(String[] args) {
+        int[] list = {1, 2, 3, 4};
+        for (int x : list) {
+            System.out.println(x);
+        }
+    }
+}
